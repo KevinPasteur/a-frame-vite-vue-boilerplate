a846e562dfd2beb754b21ab8ee16fdc54d062b31
Kévin <kevin.pasteur@icloud.com>
2024-03-03 22:09:53 +0100

    Add pokemon data

---
+++ src/utils/pokemonData.js
@@ -0,0 +1,17 @@
+const pokemonAttacksDB = {
+  charmander: [
+    { name: "Lance-flamme", damage: 25, color: "red" },
+    { name: "Griffe", damage: 5, color: "grey" },
+  ],
+  squirtle: [
+    { name: "Pistolet a eau", damage: 20, color: "blue" },
+    { name: "Morsure", damage: 10, color: "grey" },
+  ],
+  bulbasaur: [
+    { name: "Tranch'herbe", damage: 15, color: "green" },
+    { name: "Charge", damage: 10, color: "grey" },
+  ],
+  // Ajoutez d'autres Pokémon et leurs attaques ici
+};
+
+export { pokemonAttacksDB };
